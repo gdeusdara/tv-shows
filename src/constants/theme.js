958c2520a068fcc95f6187601540d55dd1251557
Guilherme Deusdará <guibanci@gmail.com>
2022-11-05 15:44:12 -0300

    defining theme
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

---
+++ src/constants/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+  colors: {
+    primary: '#4b98c4',
+    text: '#e9f1f7',
+    unactive: '#3c434a',
+    secondaryText: '#848a92',
+    background: '#111820'
+  },
+  textSizes: {
+    S: 10,
+    SM: 12,
+    M: 15,
+    ML: 17,
+    L: 20,
+    LX: 22,
+    X: 25,
+    XX: 30
+  },
+  spaces: {
+    S: 5,
+    M: 10,
+    L: 20
+  }
+}
+
+export default theme
