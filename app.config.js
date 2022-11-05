f4c30a29d44007c0f851ccf34545e2dcc5bef5a6
Guilherme Deusdará <guibanci@gmail.com>
2022-11-05 12:16:38 -0300

    installing dependencies
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'MyApp',
+  version: '1.0.0',
+  extra: {
+    TV_maze_api: process.env.TV_MAZE_API,
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            "@assets": './src/assets',
+            "@components": './src/components',
+            "@hooks": './src/hooks',
+            "@constants": './src/constants',
+            "@store": './src/store',
+            "@pages": './src/pages',
+            "@routes": './src/routes',
+          },
+        },
+      ],
+    ],
   };
 };
